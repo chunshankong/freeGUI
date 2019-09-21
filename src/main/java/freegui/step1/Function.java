be4015c9e81f0d7b0c0438a24029e406aee7d2c4
杨思国 <yangsiguo@zhuifintech.com>
2019-09-21 09:52:47 +0800

    s

---
+++ src/main/java/freegui/step1/Function.java
@@ -0,0 +1,31 @@
+package freegui.step1;
+
+public class Function {
+
+    public static void main(String[] args) {
+        String msg = "你好";
+        log(msg);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public static void log(String msg){
+        System.out.println(msg);
+    }
+}
+
+
